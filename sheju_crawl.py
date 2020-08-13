5782473ddbef307f43bafe6677222b4ba2a30129
Jackie <13265466114@163.com>
2020-08-13 17:14:47 +0800

    1. rename

--- sheju_crawl.py
+++ sheju_crawl.py
@@ -4,7 +4,6 @@ import os
 import threading
 from concurrent.futures import ThreadPoolExecutor
 from selenium import webdriver
-import time
 
 
 def cjg_crawl(info, index="", download_list=[]):
@@ -52,8 +51,8 @@ def cjg_crawl(info, index="", download_list=[]):
             print("{} 最后一页数据已加载完成".format(info[1]))
             download_images(info[1], title, image_list)
 
-    except Exception as e:
-        print("获取url超时：", e, info)
+    except Exception as crawl_error:
+        print("获取url超时：", crawl_error, info)
 
 
 def download_images(folder, title, url_list):
@@ -61,19 +60,19 @@ def download_images(folder, title, url_list):
         os.mkdir(folder)
 
     n = 0
-
     for url in url_list:
         file_name = title + str(n + 1) + ".jpg"
-        print("%s"%(threading.current_thread().name), folder, "-"* 5, url)
+        print("%s" % (threading.current_thread().name), folder, "-"* 5, url)
+
         try:
             result = requests.get(url, timeout=120)
-            with open(f"{folder}/{file_name}", "wb") as f:
-                f.write(result.content)
+            with open(f"{folder}/{file_name}", "wb") as image_f:
+                image_f.write(result.content)
             n += 1
-        except Exception as e:
+        except Exception as download_error:
             t = (folder, url, file_name)
             exception_image_list.append(t)
-            print(folder, url, "图片下载异常", e)
+            print(folder, url, "图片下载异常", download_error)
 
     complete_list.append(folder)
     print(divide_line.format(folder + "下载完成"))
@@ -85,7 +84,6 @@ def get_all_image_set(pool):
 
     info_list = []
     for h2_tag in bs.find_all("h2"):
-        # print(h2_tag)
         link = h2_tag.a.get("href")
         title = h2_tag.a.string
         info_list.append((link, title))
@@ -96,18 +94,18 @@ def get_all_image_set(pool):
         pool.map(cjg_crawl, [info])
 
 
-def exception_download(exception_image_list):
+def exception_download(exception_list):
     print(divide_line.format("Exception download begin"))
-    if len(exception_image_list) <= 0:
+    if len(exception_list) <= 0:
         return
 
-    for image in exception_image_list:
+    for image in exception_list:
         try:
             result = requests.get(image[1], timeout=180)
-            with open("./%s/%s"%(image[0], image[2]), "wb") as f:
-                f.write(result.content)
-        except Exception as e:
-            print("Exception download 异常信息：", e, image)
+            with open("./%s/%s" % (image[0], image[2]), "wb") as exception_f:
+                exception_f.write(result.content)
+        except Exception as exception_error:
+            print("Exception download 异常信息：", exception_error, image)
     print(divide_line.format("Exception download complete"))
 
 
@@ -133,9 +131,9 @@ if __name__ == '__main__':
 
     exception_image_list, complete_list = [], []
 
-    with ThreadPoolExecutor(max_workers=15, thread_name_prefix="当前线程_") as pool:
+    with ThreadPoolExecutor(max_workers=15, thread_name_prefix="当前线程_") as thread_pool:
         print(divide_line.format("开始下载"))
-        get_all_image_set(pool)
+        get_all_image_set(thread_pool)
 
     if len(complete_list) > 0:
         print(divide_line.format("下载异常，请检查target.html文件内容"))
@@ -149,6 +147,6 @@ if __name__ == '__main__':
     else:
         print(divide_line.format("下载任务完成"))
 
-
+    # debug mode
     # t = ('/luyilu/2020/0725/9127_2.html', '某群美胸比赛无圣光套图[58P]')
     # cjg_crawl(t)
