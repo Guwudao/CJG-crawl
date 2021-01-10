6f130d68e8325ff98b595054b69343b7fdd047e8
Jackie <13265466114@163.com>
2021-01-10 11:38:40 +0800

    1. code sequence re-arrange

--- jstaotu.py
+++ jstaotu.py
@@ -4,6 +4,8 @@ import threading
 import urllib3
 from bs4 import BeautifulSoup
 
+divide_line = "—————————————————— {} ——————————————————"
+exception_image_list, complete_list = [], []
 
 def get_all_topics():
     # base_url = "http://www.jstaotu.com/"
@@ -47,23 +49,26 @@ def parse_html():
         print("target.html解析错误：", parse_error)
 
 
-def fetch_all_image_in_album():
-    url = "http://www.gqxzt.com/gaoqingr/qingzxie/20211021/111735.html"
+def fetch_all_list_for_special_topic() -> []:
+    url = "http://www.gqxzt.com/gaoqingr/qingzxies/xinyanxiaogongzhu/1.html"
     resp = requests.get(url)
     resp.encoding = "gb2312"
     bs = BeautifulSoup(resp.text, "html.parser")
-    all_pages_count = bs.find_all("div", class_="page")[0].find_all("a")
 
-    image_page_list = []
+    page_list = []
+    page_list.append(url)
+    page_a_list = bs.find_all("div", class_="pagelist")[0].find_all("a")
+    # print(page_a_list)
+
     url_prefix = url.rsplit("/", 1)[0]
-    for count in all_pages_count:
-        href = count.get("href")
-        link = url_prefix + "/" + href
-        if link not in image_page_list:
-            image_page_list.append(link)
+    for a_tag in page_a_list:
+        href = a_tag.get("href")
+        if href not in page_list:
+            full_url = os.path.join(url_prefix, href)
+            page_list.append(full_url)
 
-    print(image_page_list)
-    print(bs.find_all("img"))
+    print(page_list)
+    return page_list
 
 
 def fetch_all_album_in_page() -> []:
@@ -87,26 +92,46 @@ def fetch_all_album_in_page() -> []:
     return album_info_list
 
 
-def fetch_all_list_for_special_topic() -> []:
-    url = "http://www.gqxzt.com/gaoqingr/qingzxies/xinyanxiaogongzhu/1.html"
+def fetch_all_image_in_album():
+    url = "http://www.gqxzt.com/gaoqingr/qingzxie/20211021/111735.html"
     resp = requests.get(url)
     resp.encoding = "gb2312"
     bs = BeautifulSoup(resp.text, "html.parser")
+    all_pages_count = bs.find_all("div", class_="page")[0].find_all("a")
 
-    page_list = []
-    page_list.append(url)
-    page_a_list = bs.find_all("div", class_="pagelist")[0].find_all("a")
-    # print(page_a_list)
-
+    image_page_list = []
     url_prefix = url.rsplit("/", 1)[0]
-    for a_tag in page_a_list:
-        href = a_tag.get("href")
-        if href not in page_list:
-            full_url = os.path.join(url_prefix, href)
-            page_list.append(full_url)
+    for count in all_pages_count:
+        href = count.get("href")
+        link = url_prefix + "/" + href
+        if link not in image_page_list:
+            image_page_list.append(link)
 
-    print(page_list)
-    return page_list
+    print(image_page_list)
+    print(bs.find_all("img"))
+
+
+def download_images(folder, title, url_list):
+    if not os.path.exists(folder):
+        os.mkdir(folder)
+
+    n = 0
+    for url in url_list:
+        file_name = title + str(n + 1) + ".jpg"
+        print("%s" % threading.current_thread().name, folder, "-" * 5, url)
+
+        try:
+            result = requests.get(url, timeout=120, verify=False)
+            with open(f"{folder}/{file_name}", "wb") as image_f:
+                image_f.write(result.content)
+            n += 1
+        except Exception as download_error:
+            t = (folder, url, file_name)
+            exception_image_list.append(t)
+            print(folder, url, "图片下载异常", download_error)
+
+    complete_list.append(folder)
+    print(divide_line.format(folder + "下载完成"))
 
 
 if __name__ == '__main__':
